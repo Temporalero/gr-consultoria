62dce34f69f15c459f49ab323de4f293bf38a9c3
AledroG <alexgama9817@gmail.com>
2022-07-22 20:22:38 -0500

    ADD:: Modulo Recibos de ventas personalizados

---
+++ sale_receipt_custom/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2018 Garazd Creation (<https://garazd.biz>)
+# @author: Yurii Razumovskyi (<support@garazd.biz>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.html).
+
+{
+    'name': 'Recibos de ventas personalizados ',
+    'version': '15.0.1.0.1',
+    'category': 'sale',
+    'author': 'Ing. Alejandro Garcia Magaña',
+    'website': '',
+    'license': 'LGPL-3',
+    'summary': 'Modificaciones en los recibos de venta ',
+
+    'depends': [
+        'sale',
+        'partner_delivery_routes'
+    ],
+    'data': [
+        'report/sale_report_inherit.xml',
+    ],
+    'demo': [],
+    'external_dependencies': {
+    },
+    'support': 'support@garazd.biz',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}

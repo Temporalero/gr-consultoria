8f3c503378908171f9853d991f8bd5cfdb5315cd
AledroG <alexgama9817@gmail.com>
2022-07-24 11:02:11 -0500

    TEST :: Direccion en ticket compra

--- sale_receipt_custom/static/src/js/Orderline.js
+++ sale_receipt_custom/static/src/js/Orderline.js
@@ -22,95 +22,13 @@ odoo.define('sale_receipt_custom.Orderline', function (require) {
     var round_pr = utils.round_precision;
     var exports = {};
 
+    var models = exports.PosModel.prototype.models;
+
+    models.find(e => e.model == "res.company").fields.push('street_name','street_number','street2','l10n_mx_edi_colony','l10n_mx_edi_colony_code','city','state_id','zip');
+
 
     models.PosModel = models.PosModel.extend({
 
-        load_server_data: function(){
-            var self = this;
-            var progress = 0;
-            var progress_step = 1.0 / self.models.length;
-
-            for (let value of self.models) {
-              if(value.model == 'res.company'){
-              console.log("AGREGAMOS CAMPOS EN COMPANY")
-                value.fields = [ 'currency_id', 'email', 'website', 'company_registry', 'vat', 'name', 'phone', 'partner_id' , 'country_id', 'state_id', 'tax_calculation_rounding_method','street_name','street_number','street2','l10n_mx_edi_colony','l10n_mx_edi_colony_code','city','state_id','zip']
-              }
-
-            }
-            var tmp = {}; // this is used to share a temporary state between models loaders
-
-            var loaded = new Promise(function (resolve, reject) {
-                async function load_model(index) {
-                    if (index >= self.models.length) {
-                        resolve();
-                    } else {
-                        var model = self.models[index];
-                        self.setLoadingMessage(_t('Loading')+' '+(model.label || model.model || ''), progress);
-
-                        var cond = typeof model.condition === 'function'  ? model.condition(self,tmp) : true;
-                        if (!cond) {
-                            load_model(index+1);
-                            return;
-                        }
-
-                        var fields =  typeof model.fields === 'function'  ? model.fields(self,tmp)  : model.fields;
-                        var domain =  typeof model.domain === 'function'  ? await model.domain(self,tmp)  : model.domain;
-                        var context = typeof model.context === 'function' ? model.context(self,tmp) : model.context || {};
-                        var ids     = typeof model.ids === 'function'     ? model.ids(self,tmp) : model.ids;
-                        var order   = typeof model.order === 'function'   ? model.order(self,tmp):    model.order;
-                        progress += progress_step;
-
-                        if(model.model ){
-                            var params = {
-                                model: model.model,
-                                context: _.extend(context, self.session.user_context || {}),
-                            };
-
-                            if (model.ids) {
-                                params.method = 'read';
-                                params.args = [ids, fields];
-                            } else {
-                                params.method = 'search_read';
-                                params.domain = domain;
-                                params.fields = fields;
-                                params.orderBy = order;
-                            }
-
-                            self.rpc(params).then(function (result) {
-                                try { // catching exceptions in model.loaded(...)
-                                    Promise.resolve(model.loaded(self, result, tmp))
-                                        .then(function () { load_model(index + 1); },
-                                            function (err) { reject(err); });
-                                } catch (err) {
-                                    console.error(err.message, err.stack);
-                                    reject(err);
-                                }
-                            }, function (err) {
-                                reject(err);
-                            });
-                        } else if (model.loaded) {
-                            try { // catching exceptions in model.loaded(...)
-                                Promise.resolve(model.loaded(self, tmp))
-                                    .then(function () { load_model(index +1); },
-                                        function (err) { reject(err); });
-                            } catch (err) {
-                                reject(err);
-                            }
-                        } else {
-                            load_model(index + 1);
-                        }
-                    }
-                }
-
-                try {
-                    return load_model(0);
-                } catch (err) {
-                    return Promise.reject(err);
-                }
-            });
-
-            return loaded;
-        },
 
         get_currency_text: async function(amount) {
             console.log("##Order##");

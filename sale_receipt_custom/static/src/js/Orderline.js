12dbc320a36e07fbe88cba87c7b1738ee0fc6afa
AledroG <alexgama9817@gmail.com>
2022-07-24 09:33:46 -0500

    Update Orderline with then

--- sale_receipt_custom/static/src/js/Orderline.js
+++ sale_receipt_custom/static/src/js/Orderline.js
@@ -17,7 +17,7 @@ odoo.define('sale_receipt_custom.Orderline', function (require) {
 
             return this.rpc(params).then( value => {
                 console.log(value) //log the returned value
-                return value; // returning the value from a then function returns a new promise, so the spell function also returns a promise which you can handle similarly
+                return Promise.all([value]); // returning the value from a then function returns a new promise, so the spell function also returns a promise which you can handle similarly
               });
 
 

788716e85a64935909bfc4c59ecbc6fcf989ff6a
AledroG <alexgama9817@gmail.com>
2022-07-24 12:27:18 -0500

    Update sale_order.py

--- sale_receipt_custom/models/sale_order.py
+++ sale_receipt_custom/models/sale_order.py
@@ -46,7 +46,9 @@ class PosOrderTextAmount(models.Model):
     def get_currency_to_text(self, vals):
         _log.info("Valores %s", vals)
         _log.info(vals)
-        text = self.env['res.company'].search([('id','=',vals['company_id'])]).currency_id.amount_to_text(vals['amount'])
+        company = self.env['res.company'].search([('id','=',vals['company_id'])])
+        _log.info("company %s, currency",company,company.currency_id)
+        text = company.currency_id.amount_to_text(vals['amount'])
         _log.info("Val text %s",text)
         return text
 
